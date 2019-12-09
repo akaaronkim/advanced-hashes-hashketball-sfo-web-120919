64f6c432ee3a9afaeed565d769ade2084602dc69
Aaron Kim <akaaronkim@gmail.com>
2019-12-09 08:39:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,125 +1,118 @@
+require 'pry'
+
 def game_hash
-  {
+  hash = {
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [{
-        :player_name => "Alan Anderson",
-        :number => 0,
-        :shoe => 16,
-        :points => 22,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 3,
-        :blocks => 1,
-        :slam_dunks => 1
-      },
-      {
-        :player_name => "Reggie Evans",
-        :number => 30,
-        :shoe => 14,
-        :points => 12,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 12,
-        :blocks => 12,
-        :slam_dunks => 7
-      },
-      {
-        :player_name => "Brook Lopez",
-        :number => 11,
-        :shoe => 17,
-        :points => 17,
-        :rebounds => 19,
-        :assists => 10,
-        :steals =>3,
-        :blocks => 1,
-        :slam_dunks => 15
-      },
-      {
-        :player_name => "Mason Plumlee",
-        :number => 1,
-        :shoe => 19,
-        :points => 26,
-        :rebounds => 11,
-        :assists => 6,
-        :steals => 3,
-        :blocks => 8,
-        :slam_dunks => 5
-      },
-      {
-        :player_name => "Jason Terry",
-        :number => 31,
-        :shoe => 15,
-        :points => 19,
-        :rebounds => 2,
-        :assists => 2,
-        :steals => 4,
-        :blocks => 11,
-        :slam_dunks => 1
+      :players => {
+        "Alan Anderson" => {
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        },
+        "Reggie Evans" => {
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
+        },
+        "Brook Lopez" => {
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15
+        },
+        "Mason Plumlee" => {
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebounds => 12,
+          :assists => 6,
+          :steals => 3,
+          :blocks => 8,
+          :slam_dunks => 5
+        },
+        "Jason Terry" => {
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :rebounds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1
+        }
       }
-      ]
     },
-
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
-      :players => [{
-        :player_name => "Jeff Adrien",
-        :number => 4,
-        :shoe => 18,
-        :points => 10,
-        :rebounds => 1,
-        :assists => 1,
-        :steals => 2,
-        :blocks => 7,
-        :slam_dunks => 2
-      },
-      {
-        :player_name => "Bismack Biyombo",
-        :number => 0,
-        :shoe => 16,
-        :points => 12,
-        :rebounds => 4,
-        :assists => 7,
-        :steals => 22,
-        :blocks => 15,
-        :slam_dunks => 10
-      },
-      {
-        :player_name => "DeSagna Diop",
-        :number => 2,
-        :shoe => 14,
-        :points => 24,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 4,
-        :blocks => 5,
-        :slam_dunks => 5
-      },
-      {
-        :player_name => "Ben Gordon",
-        :number => 8,
-        :shoe => 15,
-        :points => 33,
-        :rebounds => 3,
-        :assists => 2,
-        :steals => 1,
-        :blocks => 1,
-        :slam_dunks => 0
-      },
-      {
-        :player_name => "Kemba Walker",
-        :number => 33,
-        :shoe => 15,
-        :points => 6,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 7,
-        :blocks => 5,
-        :slam_dunks => 12
+      :players => {
+        "Jeff Adrien" => {
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
+        },
+        "Bismak Biyombo" => {
+          :number => 0,
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 7,
+          :blocks => 15,
+          :slam_dunks => 10
+        },
+        "DeSagna Diop" => {
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
+        },
+        "Ben Gordon" => {
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0
+        },
+        "Brendan Haywood" => {
+          :number => 33,
+          :shoe => 15,
+          :points => 6,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 22,
+          :blocks => 5,
+          :slam_dunks => 12
+        }
       }
-      ]
     }
   }
 end
@@ -224,77 +217,41 @@ def most_points_scored
 end
 
 def winning_team
-  total_home_points = 0
-  total_away_points = 0
-
-  game_hash.each do |home_away, team|
-    if home_away == :home
-      team.each do |attribute, stats|
-        if attribute == :players
-          stats.each do |player_info|
-            total_home_points += player_info[:points]
-          end
-        end
-      end
+  total_points = 0
+  winner = ''
+  game_hash.each do |stats, team|
+    team_points = 0
+    team_name = game_hash[stats][:team_name]
+    team[:players].each do |player|
+      points = player[:points]
+      team_points += points
     end
-  total_home_points
-
-    if home_away == :away
-      team.each do |attribute, stats|
-        if attribute == :players
-          stats.each do |player_info|
-            total_away_points += player_info[:points]
-          end
-        end
-      end
-    end
-  total_away_points
-
-    if total_home_points > total_away_points
-      return game_hash[:home][:team_name]
-    else
-      return game_hash[:away][:team_name]
-    end
+    win_team, total_points = team_name, team_points if team_points > total_points
   end
+  return win_team
 end
 
 
 
 def player_with_longest_name
-  longest_name_count = 0
-  longest_name = ""
-
-  game_hash.each do |status, team|
-    team.each do |attribute, stats|
-      if attribute == :players
-        stats.each do |player_info|
-          if player_info[:player_name].length > longest_name_count
-            longest_name_count = player_info[:player_name].length
-            longest_name = player_info[:player_name]
-          end
-        end
-      end
+  longest = ''
+  longest_length = 0
+  game_hash.each do |stats, team|
+    team[:player].each do |player|
+      name_length = player[:player_name].longest
+      longest, longest_length = player[:player_name], name_length if name_length > longest_length
     end
   end
-  longest_name
+  return longest
 end
 
 def long_name_steals_a_ton?
-  highest_steals = 0
- player_with_most_steals = ""
- game_hash.each do |status, team|
-   team.each do |attribute, stats|
-     if attribute == :players
-       stats.each do |player_info|
-         if player_info[:steals] > highest_steals
-           highest_steals = player_info[:steals]
-           player_with_most_steals = player_info[:player_name]
-         end
-       end
-     end
-   end
- end
- player_with_most_steals
-
- (player_with_most_steals == player_with_longest_name) ? true : false
+  steals_most = ''
+  most_steals = 0
+  game_hash.each do |stats, team|
+    team[:players].each do |player|
+      steals_most, most_steals = player[:player_name], player[:steals] if player[:steals] > most_steals
+    end
+  end
+  return true if steals_most == player_with_longest_name
 end
